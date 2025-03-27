66594798b135f60a86aa1d2e32273d703d907259
adv-11 <advaitss11@gmail.com>
2025-03-27 14:57:07 +0530

    pipeline HF

--- gemma-3-1b-it-Q4_K_M-GGUF.py
+++ gemma-3-1b-it-Q4_K_M-GGUF.py
@@ -1,15 +1,25 @@
-from llama_cpp import Llama
+# Method 1
+# from llama_cpp import Llama
 
-llm = Llama.from_pretrained(
-	repo_id="adv-11/gemma-3-1b-it-Q4_K_M-GGUF",
-	filename="gemma-3-1b-it-q4_k_m.gguf",
-)
+# llm = Llama.from_pretrained(
+# 	repo_id="adv-11/gemma-3-1b-it-Q4_K_M-GGUF",
+# 	filename="gemma-3-1b-it-q4_k_m.gguf",
+# )
 
-llm.create_chat_completion(
-	messages = [
-		{
-			"role": "user",
-			"content": "What is the capital of France?"
-		}
-	]
-)
+# llm.create_chat_completion(
+# 	messages = [
+# 		{
+# 			"role": "user",
+# 			"content": "What is the capital of France?"
+# 		}
+# 	]
+# )
+
+
+# Method 2
+
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="adv-11/gemma-3-1b-it-Q4_K_M-GGUF")
+
+print(pipe("What is the capital of India ", max_length=50, do_sample=True, temperature=0.9))
